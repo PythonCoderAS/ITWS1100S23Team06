e97d7507326a84e73302c802d4bdb76dc2504932
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2023-03-18 23:01:04 -0400

    Setup basis of project

---
+++ code/js/dashboard.js
@@ -0,0 +1,122 @@
+const dashboard = {
+    defaultConfig: {
+        /**
+         * The components to show in the dashboard.
+         * 
+         * List of components:
+         * - date: This is the date showon on the top left.
+         * - time: This is the time shown on the top left.
+         * - weather: This is the weather shown on the top left.
+         * - bookmarks: This is the bookmarks shown on the top right.
+         * - searchBar: This is the search bar on the middle.
+         * - nextCourse: This is the time until the next course, shown on the middle.
+         * - weeklySchedule: This is the weekly schedule, shown on the bottom.
+         */
+        componentsToShow: [
+            "date",
+            "time",
+            "weather",
+            "bookmarks",
+            "searchBar",
+            "nextCourse",
+            "weeklySchedule"
+        ],
+        theme: {
+            /**
+             * The theme to use. Valid values are:
+             * 
+             * - light: The light theme.
+             * - dark: The dark theme.
+             * - system: Follows the system configuration.
+             * - timeOfDay: Follows the time of day at RPI.
+             */
+            mode: "timeOfDay",
+            /**
+             * Whether to use gradient colors for the theme if set to "timeOfDay".
+             * 
+             * If disabled, it will use the light theme before sunset and the dark theme
+             * after sunset.
+             * 
+             * Has no effect if the mode is any other values.
+             */
+            useGradientColors: true
+        }
+    },
+    currentConfig: null,
+    init: function() {
+        if (currentConfig === null){
+            currentConfig = this.loadConfig();
+        }
+        this.generateDashboard();
+    },
+    loadConfig: function() {
+        // Load the config from the local storage.
+        let config = localStorage.getItem("dashboardConfig");
+        if (config === null) {
+            // If the config is not found, use the default config.
+            config = this.defaultConfig;
+        } else {
+            // If the config is found, parse it as JSON.
+            config = JSON.parse(config);
+        }
+        return config;
+    },
+    getConfig: function(path) {
+        // Get the value of "path" from the config.
+        // In order to access nested values in the config, use dot notation.
+        // For example, to get the current theme mode, we would supply
+        // path as "theme.mode".
+        let configValue = this.currentConfig;
+        let defaultConfigValue = this.defaultConfig;
+        let pathParts = path.split(".");
+        for (const part of pathParts) {
+            configValue = configValue[part];
+            defaultConfigValue = defaultConfigValue[part];
+            if (configValue === undefined){
+                // If the config value is not found, we start using the default config value.
+                configValue = defaultConfigValue;
+            }
+        }
+        return configValue;
+        
+    },
+    saveConfig: function() {
+        // Save the config to the local storage as JSON.
+        localStorage.setItem("dashboardConfig", JSON.stringify(this.currentConfig));
+    },
+    lastDownloadURL: null,
+    generateConfigDownloadLink: function() {
+        if (this.lastDownloadURL !== null) {
+            // Revoke the old download URL.
+            URL.revokeObjectURL(this.lastDownloadURL);
+        }
+        // Create a new download URL.
+        this.lastDownloadURL = URL.createObjectURL(new Blob([JSON.stringify(this.currentConfig)], {type: "application/json"}));
+        $("#exportButton").attr("href", this.lastDownloadURL);
+        $("#exportButton").attr("download", "dashboardConfig.json");
+        return this.lastDownloadURL;
+    },
+    setupImport: function() {
+        $("#importButton").on("change", function() {
+            // When the file is selected, read it.
+            let file = this.files[0];
+            let reader = new FileReader();
+            reader.onload = function() {
+                // When the file is read, parse it as JSON.
+                let config = JSON.parse(this.result);
+                // Save the config.
+                dashboard.currentConfig = config;
+                dashboard.saveConfig();
+                // Reload the page.
+                location.reload();
+            };
+            reader.readAsText(file);
+        });
+    },
+    generateSettingsPage: function() {
+        // Todo: Generate the settings page.
+    },
+    generateDashboard: function() {
+        // Todo: Generate the dashboard.
+    }
+}
